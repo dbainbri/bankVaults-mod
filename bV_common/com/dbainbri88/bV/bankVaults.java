5a3a59ae85f24278c89dddd9bfecd304cecde45e
dbainbri88 <dbainbri88@gmail.com>
2013-09-27 19:08:30 -0400

    0.0.1 09.27.13

---
+++ bV_common/com/dbainbri88/bV/bankVaults.java
@@ -0,0 +1,30 @@
+package com.dbainbri88.bV;
+
+import com.dbainbri88.bV.lib.reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = reference.MOD_ID,name = reference.MOD_NAME, version = reference.VERSION)
+public class bankVaults {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ bV_common/com/dbainbri88/bV/lib/reference.java
@@ -0,0 +1,7 @@
+package com.dbainbri88.bV.lib;
+
+public class reference {
+     public static final String MOD_ID = "BankVaults";
+     public static final String MOD_NAME = "Bank Vaults";
+     public static final String VERSION = "0.0.1";
+}
